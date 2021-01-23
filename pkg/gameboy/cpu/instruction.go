819db7ed1351625b79ca1d65ebdd74d4feae15f4
d2verb <licprog73@gmail.com>
2021-01-23 16:28:46 +0900

    implement readOperandByte/Word helpers

--- pkg/gameboy/cpu/instruction.go
+++ pkg/gameboy/cpu/instruction.go
@@ -20,16 +20,15 @@ func newInstructionSet() map[uint16]instruction {
 			return nil
 		}),
 		0x31: newInstruction("ld SP, d16", 12, func(cpu *CPU) error {
-			data, err := cpu.bus.ReadWord(cpu.regs.PC)
+			data, err := cpu.readOperandWord()
 			if err != nil {
 				return err
 			}
-			cpu.regs.PC += 2
 			cpu.regs.SP = data
 			return nil
 		}),
 		0xc3: newInstruction("jp a16", 16, func(cpu *CPU) error {
-			address, err := cpu.bus.ReadWord(cpu.regs.PC)
+			address, err := cpu.readOperandWord()
 			if err != nil {
 				return err
 			}
@@ -37,14 +36,12 @@ func newInstructionSet() map[uint16]instruction {
 			return nil
 		}),
 		0xf0: newInstruction("ldh A, (a8)", 12, func(cpu *CPU) error {
-			offset, err := cpu.bus.ReadByte(cpu.regs.PC)
+			offset, err := cpu.readOperandByte()
 			if err != nil {
 				return err
 			}
-			cpu.regs.PC++
 
-			address := 0xff00 + (uint16)(offset)
-			data, err := cpu.bus.ReadByte(address)
+			data, err := cpu.bus.ReadByte(0xff00 + (uint16)(offset))
 			if err != nil {
 				return err
 			}
@@ -57,4 +54,22 @@ func newInstructionSet() map[uint16]instruction {
 			return nil
 		}),
 	}
+}
+
+func (c *CPU) readOperandByte() (uint8, error) {
+	data, err := c.bus.ReadByte(c.regs.PC)
+	if err != nil {
+		return 0, err
+	}
+	c.regs.PC++
+	return data, nil
+}
+
+func (c *CPU) readOperandWord() (uint16, error) {
+	data, err := c.bus.ReadWord(c.regs.PC)
+	if err != nil {
+		return 0, err
+	}
+	c.regs.PC += 2
+	return data, nil
 }

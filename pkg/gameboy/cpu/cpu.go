f38076852439caaa78de315805b217638341d774
d2verb <licprog73@gmail.com>
2021-06-26 14:31:09 +0900

    Inprelemnts interrupt handler

--- pkg/gameboy/cpu/cpu.go
+++ pkg/gameboy/cpu/cpu.go
@@ -10,11 +10,12 @@ import (
 const Hz = 4194304
 
 const (
-	IntVBlank = 1 << 0
-	IntLCD    = 1 << 1
-	IntTimer  = 1 << 2
-	IntSerial = 1 << 3
-	IntJoypad = 1 << 4
+	IntVBlank   = 1 << 0
+	IntLCD      = 1 << 1
+	IntTimer    = 1 << 2
+	IntSerial   = 1 << 3
+	IntJoypad   = 1 << 4
+	intSentinel = 5 // Just a sentinel, not actual interrupt flag
 )
 
 type CPU struct {
@@ -53,6 +54,7 @@ func (c *CPU) Step() (int, error) {
 
 	instAddr := c.regs.PC
 
+	// Fetch opcode
 	opcode, err := c.fetch()
 	if err != nil {
 		return 0, err
@@ -65,9 +67,19 @@ func (c *CPU) Step() (int, error) {
 
 	log.Verbosef("(cpu) [0x%04x]: %s\n", instAddr, instruction.mnemonic)
 
+	// Execute instruction
 	cycles, err := instruction.handler(c)
 	if err != nil {
 		return 0, err
+	}
+
+	// Handle interrupts
+	if c.ime {
+		cyclesForInterrupts, err := c.handleInterrupts()
+		if err != nil {
+			return cycles, err
+		}
+		cycles += cyclesForInterrupts
 	}
 
 	return cycles, nil
@@ -112,4 +124,68 @@ func (c *CPU) ReadUInt16(address uint16) (uint16, error) {
 
 func (c *CPU) WriteUInt16(address uint16, data uint16) error {
 	return fmt.Errorf("CPU cannot be accessed at 0x%04x", address+1)
+}
+
+func (c *CPU) handleInterrupts() (int, error) {
+	filteredFlags := c.ie & c._if
+	accumulatedCycles := 0
+	for i := 0; i < intSentinel; i++ {
+		if filteredFlags&(1<<i) == 0 {
+			continue
+		}
+		cycles, err := c.handleInterrupt(1 << i)
+		if err != nil {
+			return accumulatedCycles, err
+		}
+		accumulatedCycles += cycles
+	}
+	return accumulatedCycles, nil
+}
+
+func (c *CPU) handleInterrupt(flag uint8) (int, error) {
+	if err := c.bus.ClearIF(flag); err != nil {
+		return 0, err
+	}
+
+	flagToAddress := map[uint8]uint16{
+		IntVBlank: 0x40,
+		IntLCD:    0x48,
+		IntTimer:  0x50,
+		IntSerial: 0x58,
+		IntJoypad: 0x60,
+	}
+
+	address, _ := flagToAddress[flag]
+
+	return c.callISR(address)
+}
+
+func (c *CPU) callISR(address uint16) (int, error) {
+	c.ime = false
+
+	// Save current PC to the stack as a return address
+	c.regs.SP = c.regs.SP - 2
+	if err := c.bus.WriteUInt16(c.regs.SP, c.regs.PC); err != nil {
+		return 0, err
+	}
+
+	// Jump to the ISR
+	c.regs.PC = address
+
+	return 12, nil
+}
+
+func (c *CPU) retISR() (int, error) {
+	c.ime = true
+
+	// Get return address from stack adn set it to PC
+	data, err := c.bus.ReadUInt16(c.regs.SP)
+	if err != nil {
+		return 0, err
+	}
+	c.regs.PC = data
+
+	c.regs.SP = c.regs.SP + 2
+
+	return 12, nil
 }
